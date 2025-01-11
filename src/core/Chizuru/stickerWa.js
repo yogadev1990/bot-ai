f5e8fa70d6ec67e72e6801ba6e9fcf81ce453f94
Randa Yoga Saputra <04031382328073@student.unsri.ac.id>
2025-01-11 19:36:38 +0700

    Update stickerWa.js

--- src/core/Chizuru/stickerWa.js
+++ src/core/Chizuru/stickerWa.js
@@ -2,15 +2,15 @@ const fs = require("fs");
 const { Sticker, StickerTypes } = require("wa-sticker-formatter");
 class StickerWa {
   static async create(bufferImage) {
-    const tempPath = `${__dirname}/../data/temp/test.png`;
-    const pathWebp = `${__dirname}/../data/temp/test.webp`;
+    const tempPath = `${__dirname}/../../../data/temp/test.png`;
+    const pathWebp = `${__dirname}/../../../data/temp/test.webp`;
     const stream = Buffer.from(bufferImage, "base64");
     fs.writeFileSync(tempPath, stream);
     if (fs.existsSync(pathWebp)) fs.unlinkSync(pathWebp);
 
     const sticker = new Sticker(tempPath, {
-      pack: "MPedia Pack", // The pack name
-      author: "M Pedia", // The author name
+      pack: "Chizuru Pack",
+      author: "Chizuru",
       type: StickerTypes.CROPPED,
       categories: ["🤩", "🎉"], // The sticker category
       id: "12345", // The sticker id
